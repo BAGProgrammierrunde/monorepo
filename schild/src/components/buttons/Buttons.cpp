5548372cea981acd139fae1cf470bfa8d5109230
Mentler, André <amentler@brockhaus-ag.de>
2024-11-01 21:55:04 +0100

    werte für da wandler angepasst nach vollständigem verlöten

--- schild/src/components/buttons/Buttons.cpp
+++ schild/src/components/buttons/Buttons.cpp
@@ -14,25 +14,25 @@ int Buttons::getDigitalValue() {
 
     if (isValueCloseTo(130)) return 1;
     if (isValueCloseTo(380)) return 2;
-    if (isValueCloseTo(620)) return 3;
+    if (isValueCloseTo(650)) return 3;
     if (isValueCloseTo(880)) return 4;
-    if (isValueCloseTo(1130)) return 5;
-    if (isValueCloseTo(1380)) return 6;
-    if (isValueCloseTo(1600)) return 7;
+    if (isValueCloseTo(1160)) return 5;
+    if (isValueCloseTo(1400)) return 6;
+    if (isValueCloseTo(1660)) return 7;
     if (isValueCloseTo(1850)) return 8;
-    if (isValueCloseTo(2100)) return 9;
-    if (isValueCloseTo(2300)) return 10;
-    if (isValueCloseTo(2470)) return 11;
-    if (isValueCloseTo(2750)) return 12;
-    if (isValueCloseTo(2950)) return 13;
-    if (isValueCloseTo(3250)) return 14;
-    if (isValueCloseTo(3410)) return 15;
+    if (isValueCloseTo(2110)) return 9;
+    if (isValueCloseTo(2350)) return 10;
+    if (isValueCloseTo(2600)) return 11;
+    if (isValueCloseTo(2830)) return 12;
+    if (isValueCloseTo(3120)) return 13;
+    if (isValueCloseTo(3450)) return 14;
+    if (isValueCloseTo(3850)) return 15;
 
     return 0;
 }
 
 bool Buttons::isValueCloseTo(int middle) {
-    int range = 80; 
+    int range = 100; 
     return (middle - range < this->currentValue && this->currentValue < middle + range);
 }
 
