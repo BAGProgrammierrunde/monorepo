4cfef72c9751e0b5f6f803fd4ef6fde85e3db54b
ir31k0 <ir31k0@gmail.com>
2024-12-10 16:43:41 +0100

    Überflüssige Includes aus der main entfernt

--- schild/src/main/main.cpp
+++ schild/src/main/main.cpp
@@ -1,24 +1,12 @@
 #include <Arduino.h>
-#include <driver/rtc_io.h>
 
 #include "Configuration.hpp"
 #include "main/Utility.hpp"
-#include "Components/Structure/Component.hpp"
-#include "Components/Button/Button.hpp"
-#include "Components/Joystick/Joystick.hpp"
-#include "Components/Display/Display.hpp"
 #include "Components/Structure/Device.hpp"
-#include "Components/TemperatureSensor/TemperatureSensor.hpp"
-#include "Components/potentiometer/Potentiometer.hpp"
-#include "Components/PassiveBuzzer/Tone.hpp"
-#include "Components/PassiveBuzzer/notes.hpp"
-#include "Components/PassiveBuzzer/PassiveBuzzer.hpp"
 
 #include "Features/deep_sleep/DeepSleep.hpp"
 #include "Features/lightup_leds/LightupLEDs.hpp"
-#include "Features/melody/Melody.hpp"
 #include "Features/print_button_pin/PrintButtonPin.hpp"
-#include "Features/test_display/TestDisplay.hpp"
 
 void setup() {
   Serial.begin(BAUD_RATE);
