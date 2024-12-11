74c8f66ce54ea471b47c1dc7afa6066c7ec628cf
Mentler, André <amentler@brockhaus-ag.de>
2024-12-11 19:01:53 +0100

    code beispiel für den battery check

--- schild/src/main/main.cpp
+++ schild/src/main/main.cpp
@@ -27,3 +27,15 @@ void loop() {
   Serial.print(".");
   delay(500);
 }
+
+/*
+const float MAX_READOUT = 4095;
+const float READOUT_CORRECTION = 50;
+const float MAX_VOLTAGE = 4.2;
+String batteryVoltage = "0V";
+void checkBattery() {
+  Serial.println("Battery Analog Read: " + String(analogRead(A0)-READOUT_CORRECTION));
+  batteryVoltage = String((float((analogRead(A0)-READOUT_CORRECTION))/MAX_READOUT) * MAX_VOLTAGE) + "V";
+  Serial.println("Battery Check: " + batteryVoltage);
+}
+*/
