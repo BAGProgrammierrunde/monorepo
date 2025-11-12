ccda1dfbe1349bff3cc0f2fa8472eda0d884b11b
ir31k0 <daniel.reiko@web.de>
2025-11-12 19:18:30 +0100

    switch colors of foreground and background

--- trex_game/components/game/game.cpp
+++ trex_game/components/game/game.cpp
@@ -8,14 +8,14 @@
 
 #define TAG "Game"
 
-#define BACKGROUND WHITE
-#define FOREGROUND SWAP16(0x7BEF)
+#define BACKGROUND_COLOR WHITE
+#define FOREGROUND_COLOR SWAP16(0x7BEF)
 
 void Game::init() {
 }
 
 void Game::loop() {
-    GAL::fill_background(BACKGROUND);
+    GAL::fill_background(BACKGROUND_COLOR);
 
     if (++shift > 180) {
         shift = 0;
@@ -23,17 +23,17 @@ void Game::loop() {
         // Update ground array here
     }
 
-    GAL::draw(cactus_1, 90, 38, -shift, BACKGROUND, FOREGROUND, 2);
-    GAL::draw(cactus_1, 90, 38, 180 + -shift, BACKGROUND, FOREGROUND, 2);
-    GAL::draw(cactus_1, 90, 38, 360 + -shift, BACKGROUND, FOREGROUND, 2);
+    GAL::draw(cactus_1, 90, 38, -shift, FOREGROUND_COLOR, BACKGROUND_COLOR, 2);
+    GAL::draw(cactus_1, 90, 38, 180 + -shift, FOREGROUND_COLOR, BACKGROUND_COLOR, 2);
+    GAL::draw(cactus_1, 90, 38, 360 + -shift, FOREGROUND_COLOR, BACKGROUND_COLOR, 2);
 
     nextStep++;
     if (nextStep == 12) {
         nextStep = 0;
     }
 
-    GAL::draw(nextStep > 6 ? dino_rightstep : dino_leftstep, 34, 36, 10, BACKGROUND, FOREGROUND, 2);
-
+    GAL::draw(nextStep < 6 ? dino_rightstep : dino_leftstep, 34, 36, 10, FOREGROUND_COLOR, BACKGROUND_COLOR, 2);
+    
     // GAL::draw(cactus_1, 90, 38, 270 + -shift, BACKGROUND, FOREGROUND, 1);
     // GAL::draw(cactus_1, 90, 38, 360 + -shift, BACKGROUND, FOREGROUND, 1);
 }
