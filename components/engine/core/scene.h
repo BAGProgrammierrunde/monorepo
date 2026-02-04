f267d82265572779b693b6f0c9e21e29513131c7
ir31k0 <daniel.reiko@web.de>
2026-02-04 18:37:35 +0100

    Fix missing body in scene

--- components/engine/core/scene.h
+++ components/engine/core/scene.h
@@ -5,6 +5,7 @@ class Scene {
   public:
     virtual ~Scene() {
     }
-    virtual void start();
+    virtual void start() {
+    }
     virtual void update(float deltaTime, bool buttonPressed) = 0;
 };
